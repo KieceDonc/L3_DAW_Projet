bc220910bd2627453db75cc857586c2f392c34aa
maxper4 <maxenceperion@gmail.com>
2022-03-16 10:48:03 +0100

    Début forum

--- client/view/php/forum.php
+++ client/view/php/forum.php
@@ -10,9 +10,74 @@
   </head>
   <body>
    
-  <?php include_once("header.php") ?>
+  <?php 
+    include_once("header.php");
+  ?>
+  
+  <div>
+    <h1> Forum </h1>
+    <?php 
+    if(isset($_GET["topic"]) && !empty($_GET["topic"]))
+    {
+        showTopic($_GET["topic"]);  //TODO: sanitize input
+    }
+    else 
+    {
+        listTopics();
+    }
+    ?>
+  </div>
 
     <!-- JS -->
     <script src="./src/client/js/shared.js"></script>
   </body>
 </html>
+
+<?php
+
+function listTopics() 
+{
+    ?>
+        <form method="get">
+            <h2> Topics </h2>
+            <table>
+            <tbody>
+            <?php 
+            $topics = array(
+                (object) array("id" => 1, "name" => "Sujet 1"),
+                (object) array("id" => 2, "name" => "Sujet 2")
+            ); //debug, get from bdd    
+
+            foreach($topics as $topic)
+            {
+                echo "<tr> <td> <button name='topic' value='{$topic->id}'> {$topic->name} </button> </td> </tr>";    
+            }
+            ?>
+            </tbody>
+            </table>
+        </form>
+    <?php
+}
+
+function showTopic($topicId) 
+{
+    $messages = array(
+                (object) array("id" => 1, "content" => "Message 1"),
+                (object) array("id" => 2, "content" => "Message 2")
+            ); //debug, get from bdd     
+    ?>
+
+    <table>
+    <tbody>
+    <?php
+    foreach($messages as $message)
+    {
+       echo "<tr> <td> {$message->content} </td> </tr>";    
+    }
+    ?>
+    </tbody>
+    </table>
+    <?php
+}
+
+?>
