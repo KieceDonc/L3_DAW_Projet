902add64f2b798442d2d695140a08fdc8b32a574
Cybermade <hamidounazim@gmail.com>
2022-03-20 23:52:43 +0100

    Small edit

--- admin/config.php
+++ admin/config.php
@@ -1,12 +1,12 @@
 <?php
 //db infos
-/*define('DB_SERVER', 'localhost');
+define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'php');
 define('DB_PASSWORD', 'php_local');
 define('DB_NAME', 'sys');
-*/
+/*
 define('DB_SERVER', '127.0.0.1');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'test2');
+define('DB_NAME', 'test2');*/
 ?>
