5973ad1d9a461d4917148998411d3b6158532f6c
maxper4 <maxenceperion@gmail.com>
2022-04-17 20:54:11 +0200

    Fix broken links

--- admin/view/js/sharedAdmin.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-
-	$('h1').click(function() {
-		location.href = "/";
-	});
-	
-	
-
-});
--- admin/view/php/header.php
+++ admin/view/php/header.php
@@ -1,10 +1,10 @@
 <nav>
-	<h1> E-lolning </h1>
+	<h1><a href="/" class="brand">E-lolning</a></h1>
 	<ul>
 		<?php $path = explode("/",$_SERVER['REQUEST_URI']); $filename = str_replace(".php", "", $path[count($path) - 1]);?>
-		<li <?php if($filename == "profile") echo "class='selected'";?>><a href="profile.php"> Profile </a></li>
-		<li <?php if($filename == "courses") echo "class='selected'";?>><a href="courses.php"> Your courses </a></li>
-		<li <?php if($filename == "courseBuilder") echo "class='selected'";?>><a href="courseBuilder.php"> Create a new course </a></li>
+		<li <?php if($filename == "profile") echo "class='selected'";?>><a href="profile"> Profile </a></li>
+		<li <?php if($filename == "courses") echo "class='selected'";?>><a href="courses"> Your courses </a></li>
+		<li <?php if($filename == "courseBuilder") echo "class='selected'";?>><a href="courseBuilder"> Create a new course </a></li>
 	</ul>
 </nav>
 <div class="content">
