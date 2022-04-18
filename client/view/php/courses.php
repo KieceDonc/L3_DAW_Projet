cdea2bbfebce8bfa2b992ac3c4be2ea0dc76e66b
RenardBOT <pevan.ers@gmail.com>
2022-04-18 13:18:31 +0200

    Added : Including courses list in the project

---
+++ client/view/php/courses.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <title>Courses</title>
+    
+    <!-- CSS -->
+    <link rel="stylesheet" href="../css/font-face.css" />
+    <link rel="stylesheet" href="../css/shared.css" />
+    <link rel="stylesheet" href="../css/courses.css" />
+  </head>
+  <body>
+    <?php 
+      require_once(realpath($_SERVER["DOCUMENT_ROOT"]) . "/client/view/php/header.php");
+    ?>
+    <h1>Courses</h1>
+
+    <!-- JS -->
+    <script src="../js/jquery.js"></script>
+    <script src="../js/shared.js"></script>
+  </body>
+</html>
