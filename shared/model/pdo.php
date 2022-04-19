9a491b1dd5e24ff63241265b1fceb227565c454e
Cybermade <hamidounazim@gmail.com>
2022-04-19 15:49:56 +0100

    Renamed + fixed

---
+++ shared/model/pdo.php
@@ -0,0 +1,21 @@
+<?php
+
+    require_once(realpath($_SERVER["DOCUMENT_ROOT"]) . "/shared/const.php");
+
+function getPDO(){
+    try
+    {
+        $conn = new PDO('mysql:'.DB_SERVER.';dbname='.DB_NAME.';charset='.DB_CHARSET, DB_USERNAME, DB_PASSWORD);
+
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        echo "Erreur de connection: " . $e->getMessage();
+    }
+    return $conn;
+
+}
+
+function closePDO($conn){
+    $conn = null;
+}
