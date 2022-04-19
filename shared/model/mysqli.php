a8f8292c090d437321a08490f602c29945dfb0e9
Cybermade <hamidounazim@gmail.com>
2022-04-19 14:54:16 +0100

    NOT USED ANYMORE

--- shared/model/mysqli.php
+++ shared/model/mysqli.php
@@ -1,18 +1,19 @@
 <?php 
-
+// NOT USED ANYMORE
+/*
   require_once(realpath($_SERVER["DOCUMENT_ROOT"]) . "/admin/config.php");
 
-  function getMysqli(){
+function getMysqli(){
     $mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
     if($mysqli === false){
-      die("ERROR: Could not connect. " . $mysqli->connect_error);
+        die("ERROR: Could not connect. " . $mysqli->connect_error);
     }
     return $mysqli;
-  }
 
-  function closeMysqli($mysqli){
+}
+
+function closeMysqli($mysqli){
     $mysqli->close();
-  }
-
-?>
+}
+*/
