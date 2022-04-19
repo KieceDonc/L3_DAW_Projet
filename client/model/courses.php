8a03367ab692029f264f8c0b2fb8d16a1def9f3a
maxper4 <maxenceperion@gmail.com>
2022-04-19 21:04:47 +0200

    Fix bdd queries for courses

--- client/model/courses.php
+++ client/model/courses.php
@@ -7,10 +7,9 @@
     
     function getCourses(){
         $conn = getPDO();
-        
+
         // QUERY - Retrieve the name of the course, name of its author and description of the course :
-        $query = "SELECT name , username , description FROM courses JOIN users u on courses.idauthor = u.id;"; 
-        $courses = $conn->prepare( $query)->execute();
+        $courses = $conn->query("SELECT name, username, description FROM courses JOIN users u on courses.idauthor = u.id;");
 
         closePDO($conn);
 
