93f7898a153c5d8306c8c204417323b704b17d7e
KieceDonc <valentin.verstracte@gmail.com>
2022-04-24 13:41:23 +0200

    Adding error 404 page

---
+++ shared/php/view/php/404.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8" />
+    <title>E-lolning</title>
+    
+    <!-- CSS -->
+    <link rel="stylesheet" href="/shared/php/view/css/404.css"/>
+  </head>
+  <body>
+    <div class="container">
+      <h1>Erreur 404</h1>
+    </div>
+    <h3>Il semblerait que vous êtes perdu</h3>
+    <p>La page que vous cherchez n'est pas disponible</p>
+    <a href="/">Retourner à la page principale</a>
+    
+  </body>
+</html>
