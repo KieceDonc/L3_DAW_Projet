0962107641530b7f85f314aa990cde5cf44d4b66
RenardBOT <pevan.ers@gmail.com>
2022-05-07 17:33:08 +0200

    Update submitCourse
    Retrieve id to make the xml file

--- admin/controller/submitCourse.php
+++ admin/controller/submitCourse.php
@@ -2,7 +2,11 @@
 require_once(realpath($_SERVER["DOCUMENT_ROOT"]) . "/admin/model/submitCourse.php");
 require_once(realpath($_SERVER["DOCUMENT_ROOT"]) . "/admin/view/php/submitCourse.php");
 function addCourse($name,$desc,$id){
-    addCourseInDB($name,$desc,$id);
+    $oui = addCourseInDB($name,$desc,$id);
+    $stringXml = '<?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE Questionnaire SYSTEM "quiz.dtd">
+    <Questionnaire chapitre="idduchapter" Nom="nomduchapter"> 
+    </Questionnaire>';
     header("Location: /admin/courses");
 }
 ?> 
--- admin/model/submitCourse.php
+++ admin/model/submitCourse.php
@@ -5,13 +5,20 @@ require_once(realpath($_SERVER["DOCUMENT_ROOT"]) . "/shared/php/const.php");
 function addCourseInDB($name,$desc,$userid){
     $conn = getPDO();
 
-    // PREPARED QUERY - Insert course in table
-    $querystring = "INSERT INTO courses (idauthor,name,description) VALUES (:id,:n,:d)";
+    // PREPARED QUERY - Insert course in table and get the id of the inserted course.
+    $querystring = "INSERT INTO courses (idauthor,name,description) VALUES (:id,:n,:d); ";
     $query = $conn->prepare( $querystring );
     $query->bindParam(':id',$userid);
     $query->bindParam(':n',$name);
     $query->bindParam(':d',$desc);
     $query->execute();
+
+    $querystring = "SELECT LAST_INSERT_ID();";
+    $query2 = $conn->prepare($querystring);
+    $query2->execute();;
+
     closePDO($conn);
+
+    return $query2->fetchAll();
 }
 ?>
