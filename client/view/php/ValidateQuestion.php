22131cd5cf7f8e9f5a933e1f2f473d707cb6bb52
Hugo <hugo.bertoux@icloud.com>
2022-05-08 14:21:33 +0200

    Update ValidateQuestion.php

--- client/view/php/ValidateQuestion.php
+++ client/view/php/ValidateQuestion.php
@@ -13,6 +13,7 @@ $cheminfichier = ($_SERVER["DOCUMENT_ROOT"]) . "/quizxml/quiz".$id.".xml";
 
 if(simplexml_load_file($cheminfichier)==true)
 {
+echo "ok";
 $xml = simplexml_load_file($cheminfichier);
 $number = $xml->xpath("//Questionnaire[@chapitre={$id}]/question");
 $number = count($number);
@@ -60,7 +61,7 @@ else {
   </head>
   <body>
 	<?php require_once(realpath($_SERVER["DOCUMENT_ROOT"]) . "/client/view/php/header.php");?>
-        <div>You have entered a question for the chapter <?php echo $id ."</div>";?>
+        <div>You have entered a question for the chapter <?php echo $choix4xml ."</div>";?>
         <a href="addQuestion?id=<?php echo $id; ?>">Re add question</a>
         </br>
         <a href="coursehome?id=<?php echo $id; ?>">Return to course</a>
